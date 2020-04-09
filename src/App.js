9c54b547c78ec437112518086f8b665af8b8fdea
Yunsuk Hong <yunze89@gmail.com>
2020-04-09 23:39:51 +0900

    amplify appsync 추가
    - post schema 추가 및 graphql query, mutation 테스트
    - 로그인 여부와 연관되어 쿼리 실행/실행 불가능 한 것 확인

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as queries from './graphql/queries';
+import * as mutations from './graphql/mutations';
+import * as subscriptions from './graphql/subscriptions';
 
 import Login from './components/login'
+import {API, graphqlOperation} from 'aws-amplify';
 
 const App = () => {
+
+    const examplePost = {
+        title: 'test post1',
+        description: 'test description1',
+        createdAt: new Date().toISOString(),
+    };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/*login component*/}
         <Login></Login>
+          <div>
+              {/*로그인 되어 있지 않으면, "no current user"로 실행되지 않음*/}
+              <button onClick={()=>API.graphql(graphqlOperation(mutations.createPost, {input:examplePost, }))}>create test post</button>
+              <button onClick={async()=>{
+                  const list = await API.graphql(graphqlOperation(queries.listPosts));
+                  console.log('queried list - ',list);
+              }}>get post list</button>
+          </div>
       </header>
     </div>
   );
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePost = /* GraphQL */ `
+  subscription OnCreatePost {
+    onCreatePost {
+      id
+      title
+      description
+      createdAt
+    }
+  }
+`;
+export const onUpdatePost = /* GraphQL */ `
+  subscription OnUpdatePost {
+    onUpdatePost {
+      id
+      title
+      description
+      createdAt
+    }
+  }
+`;
+export const onDeletePost = /* GraphQL */ `
+  subscription OnDeletePost {
+    onDeletePost {
+      id
+      title
+      description
+      createdAt
+    }
+  }
+`;
